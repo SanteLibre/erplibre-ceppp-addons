a0f50e6a18be6be51fc9c77e1c0d125cc06b9351
Mathieu Benoit <mathben@santelibre.ca>
2022-06-06 20:30:18 -0400

    [ADD] create ceppp_patient_partenaire

---
+++ ceppp_patient_partenaire/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Ceppp Patient Partenaire",
+    "category": "Uncategorized",
+    "version": "12.0.1.0",
+    "author": "SantéLibre",
+    "license": "AGPL-3",
+    "website": "https://santelibre.ca",
+    "application": True,
+    "depends": [
+    ],
+    "installable": True,
+}
