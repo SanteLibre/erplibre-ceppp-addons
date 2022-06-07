cd1f10e0837712aa3dbdc367ad40a1ab347e0dd5
Mathieu Benoit <mathben@santelibre.ca>
2022-06-06 21:14:34 -0400

    [ADD] demo_ceppp_patient_partenaire

---
+++ demo_ceppp_patient_partenaire/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Demo CEPPP patient partenaire",
+    "version": "12.0.1.0",
+    "author": "SantéLibre",
+    "license": "AGPL-3",
+    "website": "https://santelibre.ca",
+    "depends": ["ceppp_patient_partenaire"],
+    "data": ["data/ceppp_patient.xml"],
+    "installable": True,
+}
