9fcfec7650b9cc0f2c930a8e4456b39acbbc8c40
Mathieu Benoit <mathben@santelibre.ca>
2022-06-16 06:01:15 -0400

    [UPD] ceppp_patient_partenaire: can select patient with admin/recrut user

--- ceppp_patient_partenaire/models/res_partner.py
+++ ceppp_patient_partenaire/models/res_partner.py
@@ -14,3 +14,23 @@ class ResPartner(models.Model):
         string="Affiliation",
         help="Unique entity name to represent the contact.",
     )
+
+    def _get_contact_name(self, partner, name):
+        if partner.ceppp_entity:
+            return "%s, %s, %s" % (
+                partner.commercial_company_name or partner.parent_id.name,
+                partner.ceppp_entity.title(),
+                name,
+            )
+        return super(ResPartner, self)._get_contact_name(partner, name)
+
+    @api.depends(
+        "is_company",
+        "name",
+        "parent_id.name",
+        "type",
+        "company_name",
+        "ceppp_entity",
+    )
+    def _compute_display_name(self):
+        super(ResPartner, self)._compute_display_name()
