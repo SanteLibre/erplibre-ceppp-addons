cff2c9251cf078139360b34c8c8a0cd2a42c22aa
Mathieu Benoit <mathben@santelibre.ca>
2022-09-12 18:57:07 -0400

    [ADD] ceppp_patient_partenaire_migration_csv: module with data for prod - update image mathben

---
+++ ceppp_patient_partenaire_migration_csv/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "CEPPP patient partenaire migration CSV",
+    "version": "12.0.1.0",
+    "author": "SantéLibre",
+    "license": "AGPL-3",
+    "website": "https://santelibre.ca",
+    "depends": ["ceppp_patient_partenaire"],
+    "data": [],
+    "pre_init_hook": "pre_init_hook",
+    "post_init_hook": "post_init_hook",
+    "installable": True,
+}
--- ceppp_patient_partenaire_migration_csv/hooks.py
+++ ceppp_patient_partenaire_migration_csv/hooks.py
@@ -40,7 +40,7 @@ def pre_init_hook(cr):
             )
             partner.ceppp_entity = "administrateur"
 
-        # # Take super admin user
+        # Take super admin user
         users = env["res.users"].browse(2)
         users.groups_id = [
             (
@@ -67,13 +67,68 @@ def post_init_hook(cr, e):
             ) as desc_file:
                 partner.image = base64.b64encode(desc_file.read())
 
+        value_genevieve = {
+            "parent_id": partners.id,
+            "name": "Geneviève David",
+            "company_id": env.ref("base.main_company").id,
+            "ceppp_entity": "administrateur",
+            "customer": False,
+            "email": "genevieve.david@ceppp.ca",
+            "state_id": env["res.country.state"]
+            .search([("code", "ilike", "QC")])
+            .id,
+            "country_id": env.ref("base.ca").id,
+            "tz": "America/Montreal",
+        }
+        with tools.file_open(
+            "ceppp_patient_partenaire_migration_csv/static/img/genevieve_david.jpg",
+            "rb",
+        ) as desc_file:
+            value_genevieve["image"] = base64.b64encode(desc_file.read())
+
+        partner_id_genevieve = env["res.partner"].create(value_genevieve)
+
+        value_genevieve = {
+            "login": "genevieve.david@ceppp.ca",
+            "company_id": env.ref("base.main_company").id,
+            "partner_id": partner_id_genevieve.id,
+            "groups_id": [
+                (
+                    4,
+                    env.ref(
+                        "ceppp_patient_partenaire.group_ceppp_patient_partenaire_manager"
+                    ).id,
+                ),
+                (4, env.ref("base.user_root").id),
+                (4, env.ref("base.user_admin").id),
+            ],
+        }
+        user_id_genevieve = env["res.users"].create(value_genevieve)
+
+        value_santelibre = {
+            "name": "SantéLibre",
+            "company_id": env.ref("base.main_company").id,
+            "customer": False,
+            "supplier": True,
+            "is_company": True,
+            "state_id": env["res.country.state"]
+            .search([("code", "ilike", "QC")])
+            .id,
+            "country_id": env.ref("base.ca").id,
+            "tz": "America/Montreal",
+        }
+        with tools.file_open(
+            "ceppp_patient_partenaire_migration_csv/static/img/logo_santelibre.png",
+            "rb",
+        ) as desc_file:
+            value_santelibre["image"] = base64.b64encode(desc_file.read())
+        partner_id_santelibre = env["res.partner"].create(value_santelibre)
+
         partners = env["res.partner"].search([("name", "=", "Mathieu Benoit")])
         for partner in partners:
-            partner.parent_id = env.ref(
-                "demo_ceppp_patient_partenaire.partner_demo_company_santelibre"
-            )
+            partner.parent_id = partner_id_santelibre.id
             with tools.file_open(
-                "demo_ceppp_patient_partenaire/static/img/Mathieu_Benoit.jpg",
+                "ceppp_patient_partenaire_migration_csv/static/img/Mathieu_Benoit.jpg",
                 "rb",
             ) as desc_file:
                 partner.image = base64.b64encode(desc_file.read())
