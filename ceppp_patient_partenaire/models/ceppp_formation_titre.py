4f5d80bf36041a45c97768c1ef35f25f51e1026b
Mathieu Benoit <mathben@santelibre.ca>
2022-09-13 04:47:18 -0400

    [ADD] ceppp_patient_partenaire: sequence order data configuration

--- ceppp_patient_partenaire/models/ceppp_formation_titre.py
+++ ceppp_patient_partenaire/models/ceppp_formation_titre.py
@@ -4,5 +4,8 @@ from odoo import _, api, fields, models
 class CepppFormationTitre(models.Model):
     _name = "ceppp.formation_titre"
     _description = "ceppp_formation_titre"
+    _order = "sequence, id"
 
     name = fields.Char()
+
+    sequence = fields.Integer(default=1, help="Order the list")
--- ceppp_patient_partenaire/models/ceppp_implication_domaine.py
+++ ceppp_patient_partenaire/models/ceppp_implication_domaine.py
@@ -4,5 +4,8 @@ from odoo import _, api, fields, models
 class CepppImplicationDomaine(models.Model):
     _name = "ceppp.implication_domaine"
     _description = "ceppp_implication_domaine"
+    _order = "sequence, id"
 
     name = fields.Char()
+
+    sequence = fields.Integer(default=1, help="Order the list")
