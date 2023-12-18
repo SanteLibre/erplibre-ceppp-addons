d8ea474e2b6be5d91318cd2928fe96e117f260c5
Mathieu Benoit <mathben@technolibre.ca>
2023-12-18 14:07:49 -0500

    [ADD] survey_cadicee: new module to change field question to html

---
+++ survey_cadicee/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Formulaire CADICEE question HTML",
+    "summary": "Change type for question into survey to HTML",
+    "version": "12.0.1.0",
+    "author": "SantéLibre",
+    "license": "AGPL-3",
+    "website": "https://santelibre.ca",
+    "application": True,
+    "depends": [
+        "survey",
+    ],
+    "installable": True,
+}
